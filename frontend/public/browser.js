a74eda1a36fbf1526a509eb9abeae986e652b822
Oliver Hamlet <git@ortham.net>
2024-02-07 21:12:32 +0000

    Support WebAuthn Level 1 clients
    KeepassXC's browser extension doesn't implement any methods on AuthenticatorAttestationResponse, and conditional mediation is not available (but isn't used anyway).

--- frontend/public/browser.js
+++ frontend/public/browser.js
@@ -5,12 +5,12 @@ const WEBAUTHN_ALG_RS256 = -257;
 
 export async function arePasskeysSupported() {
     if (window.PublicKeyCredential &&
-        PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable &&
-        PublicKeyCredential.isConditionalMediationAvailable) {
+        PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable/* &&
+        PublicKeyCredential.isConditionalMediationAvailable*/) {
 
         const results = await Promise.all([
             PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable(),
-            PublicKeyCredential.isConditionalMediationAvailable(),
+            // PublicKeyCredential.isConditionalMediationAvailable(),
         ]);
 
         return results.every(result => result === true);
@@ -83,7 +83,7 @@ export async function generatePasskey(userHandle, username, userDisplayName) {
         clientData,
         // Firefox returns nonsense for getPublicKey(), so we can't avoid parsing the attestationObject server-side...
         attestationObject: toBase64(credential.response.attestationObject),
-        transports: credential.response.getTransports()
+        transports: credential.response.getTransports ? credential.response.getTransports() : []
     };
     console.log('Passkey data is', passkeyData);
 
